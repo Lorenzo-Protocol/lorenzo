337c2dd1a6293d89153ba7f988f20f6fd73abf76
ajen <ajen@lorenzo-protocol.xyz>
2024-04-01 13:23:17 +0800

    feat: add create-btcstaking-with-btc-proof cli

--- x/btcstaking/client/cli/tx.go
+++ x/btcstaking/client/cli/tx.go
@@ -1,30 +1,21 @@
 package cli
 
 import (
-	//"encoding/hex"
+	"encoding/hex"
 	"fmt"
-	//"strings"
 
-	//sdkmath "cosmossdk.io/math"
 	"github.com/cosmos/cosmos-sdk/client"
-	/*"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
-	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
-	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"*/
+
+	lrz "github.com/Lorenzo-Protocol/lorenzo/types"
+
 	"github.com/spf13/cobra"
 
-	//lrz "github.com/Lorenzo-Protocol/lorenzo/types"
+	"github.com/Lorenzo-Protocol/lorenzo/x/btcstaking/keeper"
 	"github.com/Lorenzo-Protocol/lorenzo/x/btcstaking/types"
 )
 
-const (
-	FlagMoniker         = "moniker"
-	FlagIdentity        = "identity"
-	FlagWebsite         = "website"
-	FlagSecurityContact = "security-contact"
-	FlagDetails         = "details"
-	FlagCommissionRate  = "commission-rate"
-)
 
 // GetTxCmd returns the transaction commands for this module
 func GetTxCmd() *cobra.Command {
@@ -36,8 +27,59 @@ func GetTxCmd() *cobra.Command {
 		RunE:                       client.ValidateCmd,
 	}
 
-	//FIXME: add command
-	cmd.AddCommand()
+	cmd.AddCommand(
+		NewCreateBTCStakingWithBTCProofCmd(),
+	)
 
+	return cmd
+}
+
+func NewCreateBTCStakingWithBTCProofCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "create-btcstaking-with-btc-proof [btc_tx_bytes] [proof]",
+		Short: "Create a new btc staking request with proof from bitcoin-cli getrawtransaction&gettxoutproof output",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			txBytes, err := hex.DecodeString(args[0])
+			if err != nil {
+				return fmt.Errorf("failed to decode tx bytes: %s", err)
+			}
+			proofRaw, err := hex.DecodeString(args[1])
+			if err != nil {
+				return fmt.Errorf("failed to decode proof bytes: %s", err)
+			}
+			merkleBlk, err := keeper.ParseMerkleBlock(proofRaw)
+			if err != nil {
+				return err
+			}
+			txIndex, proofBytes, err := keeper.ParseBTCProof(merkleBlk)
+
+			blkHdr := &merkleBlk.Header
+
+			var blkHdrHashBytes lrz.BTCHeaderHashBytes
+			tmp := blkHdr.BlockHash()
+			blkHdrHashBytes.FromChainhash(&tmp)
+
+			msg := types.MsgCreateBTCStaking{
+				Signer: clientCtx.GetFromAddress().String(),
+				StakingTx: &types.TransactionInfo{
+					Key: &types.TransactionKey{
+						Index: txIndex,
+						Hash:  &blkHdrHashBytes,
+					},
+					Transaction: txBytes,
+					Proof:       proofBytes,
+				},
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
 	return cmd
 }
