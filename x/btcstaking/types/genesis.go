b8f0ddd0f7c38efd84ebc623da00d5f64b0b713e
user <163304975+secp256r1@users.noreply.github.com>
2024-04-11 13:40:18 +0800

    feat(btcstaking): update testnet default genesis

--- x/btcstaking/types/genesis.go
+++ x/btcstaking/types/genesis.go
@@ -5,6 +5,9 @@ func DefaultGenesis() *GenesisState {
 	return &GenesisState{
 		Params: &Params{
 			BtcConfirmationsDepth: 6,
+			// 130 vbytes * 1e7
+			// native token 's precision is 1e18, but the fee rate value is increased by 1000 times, so finally multiply by 1e7.
+			BurnFeeFactor: 130 * 1e7,
 		},
 	}
 }
