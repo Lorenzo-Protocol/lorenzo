aee6b3771d24f307d73f2edfa082a5b88caf2f5e
happycoder9345 <happycoder@lorenzo-protocol.xyz>
2024-04-13 11:22:53 +0800

    finish tests

--- x/fee/keeper/keeper_test.go
+++ x/fee/keeper/keeper_test.go
@@ -1,96 +1,50 @@
-package keeper
+package keeper_test
 
 import (
-	"reflect"
 	"testing"
 
-	"github.com/Lorenzo-Protocol/lorenzo/x/fee/types"
+	"github.com/stretchr/testify/suite"
+
+	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
 	"github.com/cosmos/cosmos-sdk/codec"
-	"github.com/cosmos/cosmos-sdk/store/types"
-	"github.com/cosmos/cosmos-sdk/types"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/Lorenzo-Protocol/lorenzo/app/helpers"
+	"github.com/Lorenzo-Protocol/lorenzo/x/fee/keeper"
+	"github.com/Lorenzo-Protocol/lorenzo/x/fee/types"
 )
 
-func TestKeeper_GetParams(t *testing.T) {
-	type fields struct {
-		cdc       codec.BinaryCodec
-		storeKey  types.StoreKey
-		authority string
-	}
-	type args struct {
-		ctx types.Context
-	}
-	tests := []struct {
-		name       string
-		fields     fields
-		args       args
-		wantParams types.Params
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			k := Keeper{
-				cdc:       tt.fields.cdc,
-				storeKey:  tt.fields.storeKey,
-				authority: tt.fields.authority,
-			}
-			if gotParams := k.GetParams(tt.args.ctx); !reflect.DeepEqual(gotParams, tt.wantParams) {
-				t.Errorf("GetParams() = %v, want %v", gotParams, tt.wantParams)
-			}
-		})
-	}
+type KeeperTestSuite struct {
+	suite.Suite
+
+	cdc    *codec.LegacyAmino
+	ifr    codectypes.InterfaceRegistry
+	ctx    sdk.Context
+	keeper *keeper.Keeper
 }
 
-func TestKeeper_SetParams(t *testing.T) {
-	type fields struct {
-		cdc       codec.BinaryCodec
-		storeKey  types.StoreKey
-		authority string
-	}
-	type args struct {
-		ctx    types.Context
-		params types.Params
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			k := Keeper{
-				cdc:       tt.fields.cdc,
-				storeKey:  tt.fields.storeKey,
-				authority: tt.fields.authority,
-			}
-			if err := k.SetParams(tt.args.ctx, tt.args.params); (err != nil) != tt.wantErr {
-				t.Errorf("SetParams() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
+func TestKeeperTestSuite(t *testing.T) {
+	suite.Run(t, new(KeeperTestSuite))
 }
 
-func TestNewKeeper(t *testing.T) {
-	type args struct {
-		cdc       codec.BinaryCodec
-		storeKey  types.StoreKey
-		authority string
+func (suite *KeeperTestSuite) SetupTest() {
+	app := helpers.Setup(suite.T())
+
+	suite.cdc = app.LegacyAmino()
+	suite.ifr = app.InterfaceRegistry()
+	suite.ctx = app.BaseApp.NewContext(false, tmproto.Header{})
+	suite.keeper = app.FeeKeeper
+}
+
+func (suite *KeeperTestSuite) TestGetSetParams() {
+	params := types.Params{
+		NonFeeMsgs: []string{
+			sdk.MsgTypeURL(&types.MsgUpdateParams{}),
+		},
 	}
-	tests := []struct {
-		name string
-		args args
-		want *Keeper
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := NewKeeper(tt.args.cdc, tt.args.storeKey, tt.args.authority); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewKeeper() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	suite.NoError(suite.keeper.SetParams(suite.ctx, params), "set params failed")
+
+	expectedParams := suite.keeper.GetParams(suite.ctx)
+	suite.True(params.Equal(expectedParams), "not expected params")
 }
