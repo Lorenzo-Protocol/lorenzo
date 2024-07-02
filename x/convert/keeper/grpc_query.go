f7b14e998e1485ebc1ce94a6ede62c402bbefd58
orisalden <taramaorisky@gmail.com>
2024-07-02 17:59:11 +0800

    init: convert module

---
+++ x/convert/keeper/grpc_query.go
@@ -0,0 +1,28 @@
+package keeper
+
+import (
+	"context"
+
+	"github.com/Lorenzo-Protocol/lorenzo/x/convert/types"
+)
+
+var _ types.QueryServer = Querier{}
+
+type Querier struct {
+	*Keeper
+}
+
+func (q Querier) TokenPairs(ctx context.Context, request *types.QueryTokenPairsRequest) (*types.QueryTokenPairsResponse, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (q Querier) TokenPair(ctx context.Context, request *types.QueryTokenPairRequest) (*types.QueryTokenPairResponse, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (q Querier) Params(ctx context.Context, request *types.QueryParamsRequest) (*types.QueryParamsResponse, error) {
+	// TODO implement me
+	panic("implement me")
+}
---
+++ x/convert/keeper/msg_server.go
@@ -0,0 +1,28 @@
+package keeper
+
+import (
+	"context"
+
+	v1 "github.com/Lorenzo-Protocol/lorenzo/x/convert/types"
+)
+
+var _ v1.MsgServer = msgServer{}
+
+type msgServer struct {
+	*Keeper
+}
+
+func (m msgServer) ConvertCoin(ctx context.Context, coin *v1.MsgConvertCoin) (*v1.MsgConvertCoinResponse, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (m msgServer) ConvertERC20(ctx context.Context, erc20 *v1.MsgConvertERC20) (*v1.MsgConvertERC20Response, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (m msgServer) UpdateParams(ctx context.Context, params *v1.MsgUpdateParams) (*v1.MsgUpdateParamsResponse, error) {
+	// TODO implement me
+	panic("implement me")
+}
