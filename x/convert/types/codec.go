dd52dce19fdd9fbd21ac45d00eb0711d98eb0290
orisalden <taramaorisky@gmail.com>
2024-07-02 17:59:11 +0800

    init: convert module

---
+++ x/convert/types/codec.go
@@ -0,0 +1,26 @@
+package types
+
+import (
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/msgservice"
+	govv1beta1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
+)
+
+// RegisterInterfaces register implementations
+func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
+	registry.RegisterImplementations(
+		(*sdk.Msg)(nil),
+		&MsgConvertCoin{},
+		&MsgConvertERC20{},
+		&MsgUpdateParams{},
+	)
+	registry.RegisterImplementations(
+		(*govv1beta1.Content)(nil),
+		&RegisterCoinProposal{},
+		&RegisterERC20Proposal{},
+		&ToggleTokenConversionProposal{},
+	)
+
+	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
+}
---
+++ x/convert/types/keys.go
@@ -0,0 +1,33 @@
+package types
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+)
+
+const (
+	ModuleName = "convert"
+
+	// StoreKey to be used when creating the KVStore
+	StoreKey = ModuleName
+)
+
+// ModuleAddress is the native module address for EVM
+var ModuleAddress common.Address
+
+func init() {
+	ModuleAddress = common.BytesToAddress(authtypes.NewModuleAddress(ModuleName).Bytes())
+}
+
+const (
+	prefixTokenPair = iota + 1
+	prefixTokenPairByERC20
+	prefixTokenPairByDenom
+)
+
+var (
+	KeyPrefixTokenPair        = []byte{prefixTokenPair}
+	KeyPrefixTokenPairByERC20 = []byte{prefixTokenPairByERC20}
+	KeyPrefixTokenPairByDenom = []byte{prefixTokenPairByDenom}
+)
