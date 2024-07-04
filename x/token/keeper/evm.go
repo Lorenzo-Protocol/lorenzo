8ea913ad8574f5be77f0e88ddb6193502aec6195
orisalden <taramaorisky@gmail.com>
2024-07-04 15:55:11 +0800

    feat: evm call

--- x/token/keeper/evm.go
+++ x/token/keeper/evm.go
@@ -1,23 +1,46 @@
 package keeper
 
 import (
+	"encoding/json"
 	"math/big"
 
-	errorsmod "cosmossdk.io/errors"
-	"github.com/Lorenzo-Protocol/lorenzo/x/token/types"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/evmos/ethermint/server/config"
 	evmtypes "github.com/evmos/ethermint/x/evm/types"
+
+	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	errortypes "github.com/cosmos/cosmos-sdk/types/errors"
+
+	"github.com/Lorenzo-Protocol/lorenzo/x/token/types"
 )
 
+// BalanceOf returns the balance of an account in a given contract
 func (k Keeper) BalanceOf(
 	ctx sdk.Context,
 	abi abi.ABI,
 	contract, account common.Address,
 ) *big.Int {
-	panic("implement me")
+	res, err := k.CallEVM(ctx, abi, types.ModuleAddress, contract, false, "balanceOf", account)
+	if err != nil {
+		return nil
+	}
+
+	unpacked, err := abi.Unpack("balanceOf", res.Ret)
+	if err != nil || len(unpacked) == 0 {
+		return nil
+	}
+
+	balance, ok := unpacked[0].(*big.Int)
+	if !ok {
+		return nil
+	}
+
+	return balance
 }
 
 // CallEVM performs a smart contract method call using given args
@@ -29,9 +52,22 @@ func (k Keeper) CallEVM(
 	method string,
 	args ...interface{},
 ) (*evmtypes.MsgEthereumTxResponse, error) {
-	panic("implement me")
+	data, err := abi.Pack(method, args...)
+	if err != nil {
+		return nil, errorsmod.Wrap(
+			types.ErrABIPack,
+			errorsmod.Wrap(err, "failed to create transaction data").Error(),
+		)
+	}
+
+	resp, err := k.CallEVMWithData(ctx, from, &contract, data, commit)
+	if err != nil {
+		return nil, errorsmod.Wrapf(err, "contract call failed: method '%s', contract '%s'", method, contract)
+	}
+	return resp, nil
 }
 
+// CallEVMWithData performs a smart contract method call using contract data
 func (k Keeper) CallEVMWithData(
 	ctx sdk.Context,
 	from common.Address,
@@ -39,7 +75,57 @@ func (k Keeper) CallEVMWithData(
 	data []byte,
 	commit bool,
 ) (*evmtypes.MsgEthereumTxResponse, error) {
-	panic("implement me")
+	nonce, err := k.accountKeeper.GetSequence(ctx, from.Bytes())
+	if err != nil {
+		return nil, err
+	}
+
+	gasCap := config.DefaultGasCap
+	if commit {
+		args, err := json.Marshal(
+			evmtypes.TransactionArgs{
+				From: &from,
+				To:   contract,
+				Data: (*hexutil.Bytes)(&data),
+			})
+		if err != nil {
+			return nil, errorsmod.Wrapf(errortypes.ErrJSONMarshal, "failed to marshal tx args: %s", err.Error())
+		}
+
+		gasRes, err := k.evmKeeper.EstimateGas(sdk.WrapSDKContext(ctx), &evmtypes.EthCallRequest{
+			Args:   args,
+			GasCap: config.DefaultGasCap,
+		})
+		if err != nil {
+			return nil, err
+		}
+		gasCap = gasRes.Gas
+	}
+
+	msg := ethtypes.NewMessage(
+		from,
+		contract,
+		nonce,
+		big.NewInt(0), // amount
+		gasCap,        // gasLimit
+		big.NewInt(0), // gasFeeCap
+		big.NewInt(0), // gasTipCap
+		big.NewInt(0), // gasPrice
+		data,
+		ethtypes.AccessList{}, // AccessList
+		!commit,               // isFake
+	)
+
+	res, err := k.evmKeeper.ApplyMessage(ctx, msg, evmtypes.NewNoOpTracer(), commit)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.Failed() {
+		return nil, errorsmod.Wrap(evmtypes.ErrVMExecution, res.VmError)
+	}
+
+	return res, nil
 }
 
 func (k Keeper) monitorApprovalEvent(res *evmtypes.MsgEthereumTxResponse) error {
