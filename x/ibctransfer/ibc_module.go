50b929f9b3a8886b739bdb8f9e28d99a5bfe25f4
orisalden <taramaorisky@gmail.com>
2024-07-08 19:49:47 +0800

    feat(ibctransfer): extend ics-20 transfer

---
+++ x/ibctransfer/ibc_module.go
@@ -0,0 +1,23 @@
+package transfer
+
+import (
+	ibctransfer "github.com/cosmos/ibc-go/v7/modules/apps/transfer"
+	porttypes "github.com/cosmos/ibc-go/v7/modules/core/05-port/types"
+
+	"github.com/Lorenzo-Protocol/lorenzo/x/ibctransfer/keeper"
+)
+
+var _ porttypes.IBCModule = IBCModule{}
+
+// IBCModule defines the IBC module that wraps the ibc transfer module
+type IBCModule struct {
+	*ibctransfer.IBCModule
+}
+
+// NewIBCModule creates a new IBCModule given the keeper
+func NewIBCModule(k keeper.Keeper) IBCModule {
+	transferModule := ibctransfer.NewIBCModule(*k.Keeper)
+	return IBCModule{
+		IBCModule: &transferModule,
+	}
+}
