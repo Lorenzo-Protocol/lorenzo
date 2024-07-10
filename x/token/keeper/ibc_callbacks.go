ef6a3b3ce7e20222978a1fe2b00be81a28381d4a
orisalden <taramaorisky@gmail.com>
2024-07-10 11:14:02 +0800

    fix(token): ibc callback on receive

--- x/token/keeper/ibc_callbacks.go
+++ x/token/keeper/ibc_callbacks.go
@@ -1,9 +1,11 @@
 package keeper
 
 import (
+	errorsmod "cosmossdk.io/errors"
 	"github.com/Lorenzo-Protocol/lorenzo/x/token/types"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	errortypes "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/ibc-go/v7/modules/core/exported"
 	"github.com/ethereum/go-ethereum/common"
 
@@ -12,12 +14,41 @@ import (
 )
 
 // OnRecvPacket is post-posting logic for the IBC transfer module
-// TODO: we haven't decide what to do with post-processing
 func (k Keeper) OnRecvPacket(
 	ctx sdk.Context,
 	packet channeltypes.Packet,
 	ack exported.Acknowledgement,
 ) exported.Acknowledgement {
+	var data transfertypes.FungibleTokenPacketData
+	if err := transfertypes.ModuleCdc.UnmarshalJSON(packet.GetData(), &data); err != nil {
+		err = errorsmod.Wrapf(errortypes.ErrInvalidType, "cannot unmarshal ICS-20 transfer packet data")
+		return channeltypes.NewErrorAcknowledgement(err)
+	}
+
+	receiver, err := sdk.AccAddressFromBech32(data.Receiver)
+	if err != nil {
+		return channeltypes.NewErrorAcknowledgement(err)
+	}
+
+	// avoid extra costs for relayers.
+	ctx = ctx.WithKVGasConfig(storetypes.GasConfig{}).WithTransientKVGasConfig(storetypes.GasConfig{})
+
+	coin := k.getReceivedCoin(
+		packet.SourcePort, packet.SourceChannel,
+		packet.DestinationPort, packet.DestinationChannel,
+		data.Denom, data.Amount)
+
+	if _, err := k.ConvertCoin(sdk.WrapSDKContext(ctx), &types.MsgConvertCoin{
+		Coin: sdk.Coin{
+			Denom:  coin.Denom,
+			Amount: coin.Amount,
+		},
+		Receiver: common.BytesToAddress(receiver.Bytes()).String(),
+		Sender:   receiver.String(),
+	}); err != nil {
+		return channeltypes.NewErrorAcknowledgement(err)
+	}
+
 	return ack
 }
 
@@ -73,4 +104,39 @@ func (k Keeper) ConvertCoinFromPacket(
 	}
 
 	return nil
+}
+
+// getReceivedCoin returns the transferred coin from an ICS20 FungibleTokenPacketData
+func (k Keeper) getReceivedCoin(srcPort, srcChannel, dstPort, dstChannel, rawDenom, rawAmt string) sdk.Coin {
+	// NOTE: Denom and amount are already validated
+	amount, _ := sdk.NewIntFromString(rawAmt)
+
+	// if coin arrives at source chain, remove its prefix
+	if transfertypes.ReceiverChainIsSource(srcPort, srcChannel, rawDenom) {
+		voucherPrefix := transfertypes.GetDenomPrefix(srcPort, srcChannel)
+		unprefixedDenom := rawDenom[len(voucherPrefix):]
+
+		denom := unprefixedDenom
+
+		denomTrace := transfertypes.ParseDenomTrace(unprefixedDenom)
+		if denomTrace.Path != "" {
+			denom = denomTrace.IBCDenom()
+		}
+
+		return sdk.Coin{
+			Denom:  denom,
+			Amount: amount,
+		}
+	}
+
+	// otherwise add the prefix
+	sourcePrefix := transfertypes.GetDenomPrefix(dstPort, dstChannel)
+	prefixedDenom := sourcePrefix + rawDenom
+	denomTrace := transfertypes.ParseDenomTrace(prefixedDenom)
+	voucherDenom := denomTrace.IBCDenom()
+
+	return sdk.Coin{
+		Denom:  voucherDenom,
+		Amount: amount,
+	}
 }
