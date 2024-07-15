fed3c22cba28fe0f0683a63d9bd60e486ab3348e
happycoder9345 <98295363+happycoder9345@users.noreply.github.com>
2024-07-15 11:55:22 +0800

    register tendermint client type (#63)

--- app/modules.go
+++ app/modules.go
@@ -45,6 +45,7 @@ import (
 	ibc "github.com/cosmos/ibc-go/v7/modules/core"
 	ibcclientclient "github.com/cosmos/ibc-go/v7/modules/core/02-client/client"
 	ibcexported "github.com/cosmos/ibc-go/v7/modules/core/exported"
+	ibctm "github.com/cosmos/ibc-go/v7/modules/light-clients/07-tendermint"
 
 	/* ------------------------------ ethermint imports ----------------------------- */
 	"github.com/evmos/ethermint/x/evm"
@@ -106,6 +107,7 @@ var (
 		slashing.AppModuleBasic{},
 		feegrantmodule.AppModuleBasic{},
 		ibc.AppModuleBasic{},
+		ibctm.AppModuleBasic{},
 		upgrade.AppModuleBasic{},
 		evidence.AppModuleBasic{},
 		ibctransfer.AppModuleBasic{},
