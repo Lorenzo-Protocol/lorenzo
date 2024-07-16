92686d5d025914aea81c9e8ab31303a49c4c093b
sheldonleedev <163280205+sheldonleedev@users.noreply.github.com>
2024-07-16 15:06:30 +0800

    feat(ante): Keep the denom of Cosmos tx and evm tx fees consistent (#58)

--- x/fee/keeper/keeper.go
+++ x/fee/keeper/keeper.go
@@ -1,11 +1,14 @@
 package keeper
 
 import (
+	"fmt"
+
+	"github.com/cometbft/cometbft/libs/log"
+
+	"github.com/Lorenzo-Protocol/lorenzo/x/fee/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-
-	"github.com/Lorenzo-Protocol/lorenzo/x/fee/types"
 )
 
 // Keeper of the fee store
@@ -33,32 +36,7 @@ func NewKeeper(
 	}
 }
 
-// SetParams sets the parameters for the given context.
-//
-// ctx - Context object.
-// p - Params object to be set.
-// error - Returns an error if validation fails.
-func (k Keeper) SetParams(ctx sdk.Context, params types.Params) error {
-	if err := params.Validate(); err != nil {
-		return err
-	}
-	store := ctx.KVStore(k.storeKey)
-	bz := k.cdc.MustMarshal(&params)
-	store.Set(types.ParamsKey, bz)
-	return nil
-}
-
-// GetParams retrieves the parameters of the Keeper.
-//
-// ctx sdk.Context - Context
-// types.Params - Parameters
-// returns p types.Params - Parameters
-func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
-	store := ctx.KVStore(k.storeKey)
-	bz := store.Get(types.ParamsKey)
-	if bz == nil {
-		return params
-	}
-	k.cdc.MustUnmarshal(bz, &params)
-	return params
+// Logger returns a module-specific logger.
+func (k Keeper) Logger(ctx sdk.Context) log.Logger {
+	return ctx.Logger().With("module", fmt.Sprintf("x/%s", types.ModuleName))
 }
