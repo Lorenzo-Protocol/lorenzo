639338d94df21d2499c9f9bfb698228125361b8f
sheldonleedev <sheldonlee@lorenzo-protocol.xyz>
2024-07-24 15:33:16 +0800

    fix: fix set admin for agent module

---
+++ x/agent/keeper/agent_test.go
@@ -0,0 +1,50 @@
+package keeper_test
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (suite *KeeperTestSuite) TestSetAdmin() {
+	testCases := []struct {
+		name       string
+		args       sdk.AccAddress
+		expectErr  bool
+		validation func(sdk.AccAddress)
+	}{
+		{
+			name:      "fail - invalid address, invalid bech32",
+			args:      sdk.AccAddress("foobar"),
+			expectErr: true,
+		},
+		{
+			name:      "fail - admin already set",
+			args:      testAdmin,
+			expectErr: true,
+		},
+		{
+			name:      "success - set admin",
+			args:      sdk.AccAddress("lrz1xa40j022h2rcmnte47gyjg8688grln94pp84lc"),
+			expectErr: false,
+			validation: func(args sdk.AccAddress) {
+				admin := suite.keeper.GetAdmin(suite.ctx)
+				suite.Require().Equal(args, admin)
+			},
+		},
+	}
+	for _, tc := range testCases {
+		suite.Run(fmt.Sprintf("KeeperSetAdmin - %s", tc.name), func() {
+			suite.SetupTest()
+			err := suite.keeper.SetAdmin(suite.ctx, tc.args)
+			if tc.expectErr {
+				suite.Require().Error(err)
+			} else {
+				suite.Require().NoError(err)
+			}
+			if tc.validation != nil {
+				tc.validation(tc.args)
+			}
+		})
+	}
+}
