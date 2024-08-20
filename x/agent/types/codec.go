78a42494ba8ce431e64fe5d00c0f03a0154594f2
orisalden <taramaorisky@gmail.com>
2024-08-20 16:30:40 +0800

    amino: agent msg

--- x/agent/types/codec.go
+++ x/agent/types/codec.go
@@ -1,10 +1,27 @@
 package types
 
 import (
+	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
+
+var (
+	amino    = codec.NewLegacyAmino()
+	AminoCdc = codec.NewAminoCodec(amino)
+)
+
+func init() {
+	RegisterLegacyAminoCodec(amino)
+}
+
+func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
+	cdc.RegisterConcrete(&MsgUpdateParams{}, "lorenzo/agent/MsgUpdateParams", nil)
+	cdc.RegisterConcrete(&MsgAddAgent{}, "lorenzo/agent/MsgAddAgent", nil)
+	cdc.RegisterConcrete(&MsgEditAgent{}, "lorenzo/agent/MsgEditAgent", nil)
+	cdc.RegisterConcrete(&MsgRemoveAgent{}, "lorenzo/agent/MsgRemoveAgent", nil)
+}
 
 // RegisterInterfaces registers implementations for sdk.Msg and MsgUpdateParams in the given InterfaceRegistry.
 //
@@ -15,6 +32,7 @@ func RegisterInterfaces(registry types.InterfaceRegistry) {
 		&MsgAddAgent{},
 		&MsgEditAgent{},
 		&MsgRemoveAgent{},
+		&MsgUpdateParams{},
 	)
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
